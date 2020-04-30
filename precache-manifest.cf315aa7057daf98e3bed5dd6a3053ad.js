a2a94bd33a1b8db237c1850f9a6807b634e7b8c9
Phillip Tran <pntran@ucdavis.edu>
2020-04-30 15:06:59 -0700

    Updates

---
+++ precache-manifest.cf315aa7057daf98e3bed5dd6a3053ad.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74945760466eee79f70c47e84dfaa0d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47a08b47653cddac70f",
+    "url": "/static/css/main.3b6ac00f.chunk.css"
+  },
+  {
+    "revision": "cd9124de2e5886b9099a",
+    "url": "/static/js/2.9b5e2b4b.chunk.js"
+  },
+  {
+    "revision": "ee0b6c573819ac3bd62d7bd7f901db2d",
+    "url": "/static/js/2.9b5e2b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47a08b47653cddac70f",
+    "url": "/static/js/main.0beac9d7.chunk.js"
+  },
+  {
+    "revision": "5e448e580dd443c57b20",
+    "url": "/static/js/runtime-main.4b1cd430.js"
+  },
+  {
+    "revision": "effd317999c990ab86f550886b80b254",
+    "url": "/static/media/ConnectButton.effd3179.png"
+  },
+  {
+    "revision": "99a95dd4e901a6b1870b276106beada0",
+    "url": "/static/media/InnerCircle.99a95dd4.png"
+  },
+  {
+    "revision": "d9251c48ad34be6e57580715ba7f0bdf",
+    "url": "/static/media/MiddleButtons.d9251c48.png"
+  },
+  {
+    "revision": "2fc3c0ee9cd10f1fe7a4b38de48fcb33",
+    "url": "/static/media/MovementButton.2fc3c0ee.png"
+  },
+  {
+    "revision": "5045274a4f32df72a5d8f948a8b4c80e",
+    "url": "/static/media/MovementButtonOuterRing.5045274a.png"
+  },
+  {
+    "revision": "4c90771b816ffe275626e4f7666e6a9e",
+    "url": "/static/media/OuterRing.4c90771b.png"
+  },
+  {
+    "revision": "ce3e3fd6e629502f0c5e904d26516cc5",
+    "url": "/static/media/RotateButton.ce3e3fd6.png"
+  },
+  {
+    "revision": "0ba72823d516525e824f5894d69cd13a",
+    "url": "/static/media/UploadButton.0ba72823.png"
+  }
+]);
